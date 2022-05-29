9e0a3477b5fa579bdae4e1bbcd0442a7786ff860
Shankar <89457569+Shankarprince@users.noreply.github.com>
2022-05-29 09:43:49 +0530

    committed

---
+++ README.md
@@ -0,0 +1,2 @@
+# independenceDay
+This program shows independence day by counting 10 to 0
---
+++ countDownIndependence.js
@@ -0,0 +1,99 @@
+function start() {
+    setTimeout(() => {
+        const doc = document.querySelector("h1");
+        doc.innerText = "10";
+        setTimeout(() => {
+            const doc = document.querySelector("h1");
+            doc.innerText = "9";
+            setTimeout(() => {
+                const doc = document.querySelector("h1");
+                doc.innerText = "8";
+                setTimeout(() => {
+                    const doc = document.querySelector("h1");
+                    doc.innerText = "7";
+                    setTimeout(() => {
+                        const doc = document.querySelector("h1");
+                        doc.innerText = "6";
+                        setTimeout(() => {
+                            const doc = document.querySelector("h1");
+                            doc.innerText = "5";
+                            setTimeout(() => {
+                                const doc = document.querySelector("h1");
+                                doc.innerText = "4";
+                                setTimeout(() => {
+                                    const doc = document.querySelector("h1");
+                                    doc.innerText = "3";
+                                    setTimeout(() => {
+                                        const doc = document.querySelector("h1");
+                                        doc.innerText = "2";
+                                        setTimeout(() => {
+                                            const doc = document.querySelector("h1");
+                                            doc.innerText = "1";
+                                            setTimeout(() => {
+                                                const doc = document.querySelector("h1");
+                                                doc.innerText = "Happy Independence Day";
+                                            }, 1000 * 1);
+                                        }, 1000 * 1);
+                                    }, 1000 * 1);
+                                }, 1000 * 1);
+                            }, 1000 * 1);
+                        }, 1000 * 1);
+                    }, 1000 * 1);
+                }, 1000 * 1);
+            }, 1000 * 1);
+        }, 1000 * 1);
+    }, 1000 * 1);
+}
+
+
+// Call back hell program by creating a variable and counting down using  -- operator.
+/*
+var count = 10;
+function start() {
+    setTimeout(() => {
+        const doc = document.querySelector("h1");
+        doc.innerText = count;
+        setTimeout(() => {
+            const doc = document.querySelector("h1");
+            doc.innerText = --count;
+            setTimeout(() => {
+                const doc = document.querySelector("h1");
+                doc.innerText = --count;
+                setTimeout(() => {
+                    const doc = document.querySelector("h1");
+                    doc.innerText = --count;
+                    setTimeout(() => {
+                        const doc = document.querySelector("h1");
+                        doc.innerText = --count;
+                        setTimeout(() => {
+                            const doc = document.querySelector("h1");
+                            doc.innerText = --count;
+                            setTimeout(() => {
+                                const doc = document.querySelector("h1");
+                                doc.innerText = --count;
+                                setTimeout(() => {
+                                    const doc = document.querySelector("h1");
+                                    doc.innerText = --count;
+                                    setTimeout(() => {
+                                        const doc = document.querySelector("h1");
+                                        doc.innerText = --count;
+                                        setTimeout(() => {
+                                            const doc = document.querySelector("h1");
+                                            doc.innerText = --count;
+                                            setTimeout(() => {
+                                                const doc = document.querySelector("h1");
+                                                doc.innerText = "Happy Independence Day!!!";
+                                            }, 1000)
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}
+
+*/
